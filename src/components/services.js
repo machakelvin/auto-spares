8626e5b2fd58d79930b23bc4d8a46d7430960e6c
machakelvin <machakelvin76@gmail.com>
2024-08-06 14:25:16 +0300

    services Update

--- src/components/services.js
+++ src/components/services.js
@@ -7,19 +7,23 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            At our shop, we provide a range of high-quality services designed to
+            meet and exceed your expectations
           </p>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
+                  <div className="testimonial">
+                    <div className="testimonial-image">
+                      {" "}
+                      <img src={d.img} alt="" />{" "}
+                    </div>
+                    <div className="testimonial-content">
+                      <p>"{d.text}"</p>
+                      <div className="testimonial-meta"> - {d.name} </div>
+                    </div>
                   </div>
                 </div>
               ))
